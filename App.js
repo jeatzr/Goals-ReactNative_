5d75644472a247bc14d533d6fdaef3f5564353a0
jeatzr <profesor.jeatienzar@iesazarquiel.es>
2024-01-29 09:14:04 +0100

    GoalInput component

--- App.js
+++ App.js
@@ -1,43 +1,25 @@
 import { View, TextInput, Button, Text, StyleSheet, FlatList } from 'react-native';
 import { useState } from 'react';
+import GoalInput from './components/GoalInput';
 
 
 export default function App() {
-
   // declaramos el hook de estado de componente "newGoal"
-  const [newGoal, setNewGoal] = useState("");
   const [myGoals, setMyGoals] = useState([]);
 
-  function textChangeHandler(enteredText) {
-    setNewGoal(enteredText);
-  }
-
-  function addGoalHandler() {
+  function addGoalHandler(newGoalText) {
     setMyGoals(myCurrentGoals => [...myCurrentGoals,
     {
       id: Date.now(),
-      text: newGoal,
+      text: newGoalText,
     }]);
-    setNewGoal("");
-
   }
 
   return (
     <View style={styles.container}>
-      <View style={styles.inputContainer}>
-        <TextInput
-          onChangeText={textChangeHandler}
-          style={styles.textInput}
-          value={newGoal}
-          placeholder='Input your Goal!!'
-        />
-        <Button
-          title='Add Goal'
-          onPress={addGoalHandler}
-        />
-      </View>
-      <View style={styles.goalsContainer}>
+      <GoalInput onNewGoal={addGoalHandler} />
 
+      <View style={styles.goalsContainer}>
         <FlatList
           data={myGoals}
           renderItem={(dataItem) => (
@@ -47,11 +29,6 @@ export default function App() {
           )
           }
         />
-
-
-
-
-
       </View>
     </View>
   );
@@ -62,15 +39,6 @@ const styles = new StyleSheet.create({
     flex: 1,
     paddingTop: 70,
     paddingHorizontal: 15
-  },
-  inputContainer: {
-    flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    marginBottom: 20,
-    borderBottomWidth: 1,
-    borderBottomColor: "#CCCCCC",
-    alignItems: 'center'
   },
   textInput: {
     borderColor: "#CCCCCC",
